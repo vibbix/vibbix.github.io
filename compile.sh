6d84e6b20675d7da49d0a1cfaf17db8bb3099342
Mark Beznos <mark.vibbix@gmail.com>
2017-01-23 11:56:43 -0500

    Set GIT_DEPLOY_REPO

--- compile.sh
+++ compile.sh
@@ -1,4 +1,3 @@
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-git remote set-url origin $SSH_REPO
+git remote set-url origin git@github.com:vibbix/vibbix.github.io.git
 git remote -v
 hugo -t hugo-future-imperfect -b https://mtechnic.me/
