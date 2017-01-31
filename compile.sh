47b47b52ec70b51a8476c3d41f4d7ca171778c29
Mark Beznos <mark.vibbix@gmail.com>
2017-01-30 21:56:19 -0500

    Updated compile.sh to enable gitinfo building
    -gitinfo building forced by flag

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 git remote set-url origin git@github.com:vibbix/vibbix.github.io.git
 git remote -v
-hugo -t hugo-future-imperfect -b https://mtechnic.me/
+hugo -t hugo-future-imperfect -b https://mtechnic.me/ --enableGitInfo
